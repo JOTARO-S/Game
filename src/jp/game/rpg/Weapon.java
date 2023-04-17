f4dc878c34eab9714881aa88bb21a0b6bd3a8bb4
JOTARO-S <88357567+JOTARO-S@users.noreply.github.com>
2023-04-17 16:32:27 +0900

    Weapon

--- src/jp/game/rpg/Weapon.java
+++ src/jp/game/rpg/Weapon.java
@@ -1,5 +1,25 @@
 package jp.game.rpg;
 
+/*
+
+class Car2 {
+	String color;
+	int num;
+	double gas;
+	
+	public Car2(String c, int n, double g) {
+		color = c;
+		num = n;
+		gas = g;
+	}
+	
+	public void show() {
+		System.out.println("車の色は" + color + " ナンバーは[" + num + "] 燃料は [" + gas + "]");
+	}
+}
+
+*/
+
 public class Weapon {
     private String name;
     private int attack;
