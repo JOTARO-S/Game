25563e3484d44e413ada312fcbfc25e5c58e0238
JOTARO-S <88357567+JOTARO-S@users.noreply.github.com>
2023-04-21 16:02:37 +0900

    update

--- src/jp/game/rpg/GachaSystem.java
+++ src/jp/game/rpg/GachaSystem.java
@@ -1,4 +1,6 @@
 package jp.game.rpg;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -6,9 +8,9 @@ import java.util.Scanner;
  class User {
 	private int id;
 	private String name;
-	private int kakin;
+	private long kakin;
 	
-	public User(int id, String name, int kakin) {
+	public User(int id, String name, long kakin) {
 		this.id = id;
 		this.name = name;
 		this.kakin = kakin;
@@ -21,7 +23,7 @@ import java.util.Scanner;
 	String getName() {
 		return name;
 	}
-	int getKakin() {
+	long getKakin() {
 		return kakin;
 	}
 	
@@ -37,6 +39,9 @@ import java.util.Scanner;
 //ガチャシステム クラス
 public class GachaSystem extends User{
     private int magicStoneCount;
+    private int superRareItemCount;
+    private int rareItemCount;
+    private int nomalItemCount;
 
     public GachaSystem(int id, String name, int kakin, int magicStoneCount) {
     	super(id, name, kakin);
@@ -47,11 +52,17 @@ public class GachaSystem extends User{
     int getMagicStoneCount() {
     	return magicStoneCount;
     }
+    void getItemCount() {
+    	System.out.println("超レアアイテムが出た数: " + superRareItemCount);
+    	System.out.println("レアアイテムが出た数: " + rareItemCount);
+    	System.out.println("ノーマルアイテムが出た数: " + nomalItemCount);
+    }
     
     // setter(1万円を課金して魔法石を購入する)
     void setMagicStoneCount() {
     	System.out.println("1万円で魔法石を100個購入しました。");
     	this.magicStoneCount += 100;
+    	setKakin(10000);
     }
 
     // ガチャを引く
@@ -65,17 +76,20 @@ public class GachaSystem extends User{
 
         Random random = new Random();
         int result = random.nextInt(100);  // 0~99の整数をランダムに生成
-        int superRareItem = 3; //超レアアイテムの確率
-        int rareItem = 20; //レアアイテムの確率
+        int superRareItem = 1; //超レアアイテムの確率
+        int rareItem = 10; //レアアイテムの確率
 
         if (result < superRareItem) {
             System.out.println("超レアアイテムを獲得しました！");
+            superRareItemCount++;
             // 超レアアイテムの付与処理
         } else if (result < rareItem) {
             System.out.println("レアアイテムを獲得しました！");
+            rareItemCount++;
             // レアアイテムの付与処理
         } else {
             System.out.println("ノーマルアイテムを獲得しました！");
+            nomalItemCount++;
             // ノーマルアイテムの付与処理
         }
     }
@@ -91,28 +105,50 @@ public class GachaSystem extends User{
     		}
     	}
     }
+    public void playGacha100() {
+    	if(magicStoneCount < 500) {
+    		System.out.println("魔法石が足りません。");
+    		return;
+    	} else {
+    		for(int i = 0; i < 100; i++) {
+    			playGacha();
+    		}
+    	}
+    }
+    
+    
     
     // メイン
     public static void main(String[] args) {
     	GachaSystem user1 = new GachaSystem(1234, "test", 0, 100);
     	Scanner scanner = new Scanner(System.in);
+    	LocalDateTime now = LocalDateTime.now();
+    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH:mm:ss");
     	int choice = 0;
 
     	while (choice != 3) {
     	System.out.println();
+    	System.out.println(now.withNano(0).format(formatter));
     	System.out.println("あなたのユーザーID: " + user1.getId());
     	System.out.println("あなたの名前: " + user1.getName());
     	System.out.println("あなたの所持魔法石: " + user1.getMagicStoneCount());
     	System.out.println("あなたの使った金額: " + user1.getKakin() + "円");
-    	System.out.println();	
-    	System.out.println("1: ガチャを引く");
-        System.out.println("2: 10連ガチャを引く");
-        System.out.println("3: やめる");
-        System.out.println("4: 課金する");
-        System.out.println();	
-        System.out.print("選択肢を入力してください: ");
-        System.out.println();	
-        
+    	user1.getItemCount();
+    	System.out.println();
+        if(user1.getKakin() >= 1000000) {
+            System.out.println("お金の使い過ぎで人生が終わった！！");
+            System.out.println("GAME OVER");
+            break;
+        } else {
+        	System.out.println("1: ガチャを引く");
+            System.out.println("2: 10連ガチャを引く");
+            System.out.println("3: やめる");
+            System.out.println("4: 課金する");
+            System.out.println("5: 勇気の決断...10万だ");
+            System.out.println();
+        	System.out.print("選択肢を入力してください: ");
+            System.out.println();
+        }
         try {
             choice = scanner.nextInt();
         } catch (Exception e) {
@@ -130,13 +166,18 @@ public class GachaSystem extends User{
                 System.out.println("終了します");
                 break;
             case 4: //課金する
-            	user1.setKakin(10000);
             	user1.setMagicStoneCount();
+            case 5: //廃課金の末路
+            	for(int i = 0; i < 10; i++) {
+            	user1.setMagicStoneCount();
+            	}
             	break;
-            case 29:
-            	System.out.println("隠しコマンド");
-            	System.out.println("焼肉を食べに行った!とても美味しかった!");
-            	user1.setKakin(5000);
+            case 10:
+            	user1.playGacha100();
+            	break;
+            case 100:
+            	System.out.println("100万円を散財した");
+            	user1.setKakin(1000000);
             	break;
             default:
                 System.out.println("無効な選択肢です。もう一度入力してください。");
